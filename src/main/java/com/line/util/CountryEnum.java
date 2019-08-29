66724a6851f778990b4e742b8bc2b0459370c356
line_xiaowu <line_xiaowu@163.com>
2019-08-29 18:02:29 +0800

    枚举demo

---
+++ src/main/java/com/line/util/CountryEnum.java
@@ -0,0 +1,36 @@
+package com.line.util;
+
+/**
+ * @desc TODO
+ * @Author xw
+ * @Date 2019/8/29
+ */
+public enum CountryEnum {
+    OWE(1, "齐"),TWO(2,"楚"),THREE(3,"燕"),FOUR(4,"韩"),FIVE(5,"赵"),SIX(6,"魏");
+
+    private Integer retCode;
+    private String retMessage;
+
+    CountryEnum(Integer retCode, String retMessage) {
+        this.retCode = retCode;
+        this.retMessage = retMessage;
+    }
+
+    public static CountryEnum foreach_CountryEnum(int index) {
+        CountryEnum[] arr = CountryEnum.values();
+        for (CountryEnum element : arr) {
+            if (index == element.getRetCode()) {
+                return element;
+            }
+        }
+        return null;
+    }
+
+    public Integer getRetCode() {
+        return retCode;
+    }
+
+    public String getRetMessage() {
+        return retMessage;
+    }
+}
